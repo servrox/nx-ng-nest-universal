26ecb94769f86163e4d1299734b8648237d4be2f
servrox <servrox@live.com>
2019-05-27 01:43:12 +0200

    :tada: Added angular app to workspace.

---
+++ apps/ng-test-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ss-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-test-app';
+}
